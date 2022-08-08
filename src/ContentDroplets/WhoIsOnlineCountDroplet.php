6561a8625534884d51cada5ed89d39f80746c184
miriam schlindwein <schlindwein@hallowelt.com>
2022-08-08 11:21:54 +0200

    Add droplets whoisonline
    Add whoisonline count and whoisonline popup
    
    ERM29400
    
    [4.x]
    
    Change-Id: I6426cb479cff4ab68374bf591af485fe2a97386a

---
+++ src/ContentDroplets/WhoIsOnlineCountDroplet.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace BlueSpice\WhoIsOnline\ContentDroplets;
+
+use MediaWiki\Extension\ContentDroplets\Droplet\TagDroplet;
+use Message;
+use RawMessage;
+
+class WhoIsOnlineCountDroplet extends TagDroplet {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getName(): Message {
+		return new RawMessage( 'WhoIsOnlineCount' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getDescription(): Message {
+		return new RawMessage( "WhoIsOnlinecount description" );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getIcon(): string {
+		return 'userGroup';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRLModule(): string {
+		return 'ext.bluespice.whoisonlinecount.visualEditorTagDefinition';
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getCategories(): array {
+		return [ 'data' ];
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	protected function getTagName(): string {
+		return 'bs:whoisonlinecount';
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getAttributes(): array {
+		return [];
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function hasContent(): bool {
+		return false;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getVeCommand(): ?string {
+		return 'whoisonlinecountCommand';
+	}
+
+}
