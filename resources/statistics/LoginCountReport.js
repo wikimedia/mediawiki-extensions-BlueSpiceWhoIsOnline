930949dea2444dc6bad0def1f3ba502f175312a9
miriam schlindwein <schlindwein@hallowelt.com>
2022-10-07 10:05:16 +0200

    Add aggregated statistics login per day
    Removed login duration per each user
    
    ERM29537
    
    [4.2.x]
    
    Change-Id: I9ad24d911c533455aa053183dc9c8746ad9ff635
    (cherry picked from commit 245699b0acfef5cc1107d38ae96f350f9698216f)

---
+++ resources/statistics/LoginCountReport.js
@@ -0,0 +1,26 @@
+(function ( mw, $, bs) {
+	bs.util.registerNamespace( 'bs.whoisonline.report' );
+
+	bs.whoisonline.report.LoginCountReport = function ( cfg ) {
+		bs.whoisonline.report.LoginCountReport.parent.call( this, cfg );
+	};
+
+	OO.inheritClass( bs.whoisonline.report.LoginCountReport, bs.aggregatedStatistics.report.ReportBase );
+
+	bs.whoisonline.report.LoginCountReport.static.label = mw.message( 'bs-whoisonline-statistics-report-login-number' ).text();
+
+	bs.whoisonline.report.LoginCountReport.prototype.getFilters = function () {
+		return [];
+	};
+
+	bs.whoisonline.report.LoginCountReport.prototype.getChart = function () {
+		return new bs.aggregatedStatistics.charts.LineChart();
+	};
+
+	bs.whoisonline.report.LoginCountReport.prototype.getAxisLabels = function () {
+		return {
+			value: mw.message( "bs-whoisonline-statistics-report-login-number-axis-value" ).text()
+		};
+	};
+
+} )( mediaWiki, jQuery , blueSpice);
