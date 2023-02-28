ba7bb0cfe061b6390aedfa138f4f9167630470c0
rvogel <vogel@hallowelt.biz>
2023-02-28 16:16:53 +0100

    Insert trace in deferred update
    This avoids write DB operations during the regular processing of the
    request.
    
    ERM31650
    
    [4.2.x]
    
    Change-Id: I190bde4ecebc8752173ac6fb8622eab51dcf8c62

---
+++ src/Hook/BeforePageDisplay/InsertTrace.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BlueSpice\WhoIsOnline\Hook\BeforePageDisplay;
+
+use DeferredUpdates;
+
+class InsertTrace extends \BlueSpice\Hook\BeforePageDisplay {
+
+	/**
+	 *
+	 * @return bool
+	 */
+	protected function doProcess() {
+		$services = $this->getServices();
+		$context = $this->getContext();
+		DeferredUpdates::addCallableUpdate( function () use ( $services, $context ) {
+			$services->getService( 'BSWhoIsOnlineTracer' )->trace( $context );
+		} );
+		return true;
+	}
+
+}
