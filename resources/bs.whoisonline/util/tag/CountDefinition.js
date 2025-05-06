1a7684c712d47791c2fed122ce32c015e214350e
hslater <hamish.slater@hallowelt.com>
2025-05-06 16:38:34 +1200

    eslint
    ERM20742
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: I5ed36ede36f84f5e94549b3ed6acb556a42f7933

--- resources/bs.whoisonline/util/tag/CountDefinition.js
+++ resources/bs.whoisonline/util/tag/CountDefinition.js
@@ -5,15 +5,15 @@ bs.whoisonline.util.tag.CountDefinition = function BsVecUtilTagCountDefinition()
 
 OO.inheritClass( bs.whoisonline.util.tag.CountDefinition, bs.vec.util.tag.Definition );
 
-bs.whoisonline.util.tag.CountDefinition.prototype.getCfg = function() {
-	var cfg = bs.whoisonline.util.tag.CountDefinition.super.prototype.getCfg.call( this );
-	return $.extend( cfg, {
-		classname : 'Whoisonlinecount',
+bs.whoisonline.util.tag.CountDefinition.prototype.getCfg = function () {
+	const cfg = bs.whoisonline.util.tag.CountDefinition.super.prototype.getCfg.call( this );
+	return $.extend( cfg, { // eslint-disable-line no-jquery/no-extend
+		classname: 'Whoisonlinecount',
 		name: 'whoisonlinecount',
 		tagname: 'bs:whoisonlinecount',
 		descriptionMsg: 'bs-whoisonline-tag-whoisonlinecount-description',
 		menuItemMsg: 'bs-whoisonline-ve-whoisonlinecountinspector-title'
-	});
+	} );
 };
 
 bs.vec.registerTagDefinition(
